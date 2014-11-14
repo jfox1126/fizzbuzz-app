9abb8efd7a973d92abb3fd016930374aefdffc47
Jared Jones <jfox1126@gmail.com>
2014-11-14 15:46:41 -0800

    app works

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
-
+	for (i = 1; i<=100; i++) {
+		var printItem
+		if (i % 15 == 0) {
+			console.log("fizzbuzz");
+			printItem = '<div>Fizzbuzz</div>';
+			$('.printout').append(printItem);
+		} else if (i % 3 == 0) {
+			console.log("fizz");
+			printItem = '<div>Fizz</div>';
+			$('.printout').append(printItem);
+		} else if (i % 5 == 0) {
+			console.log ("buzz");
+			printItem = '<div>Buzz</div>';
+			$('.printout').append(printItem);
+		} else {
+			console.log(i)
+			printItem = '<div>' + i +'</div>';
+			$('.printout').append(printItem);
+		}
+	};
 })
+
